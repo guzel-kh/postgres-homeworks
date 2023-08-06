5dfb8839e9c08e6a6092b5454c8d0c4da4a6ab51
guzel <guzel.kh23@gmail.ru>
2023-08-06 14:22:00 +0300

    fixed

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -14,14 +14,12 @@ SELECT AVG(shipped_date - order_date)
 FROM orders
 WHERE ship_country = 'Germany';
 
--- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи
-(таблица products, колонки unit_price, discontinued не равно 1)
+-- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
 WHERE discontinued <> 1;
 
--- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20
-(таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+-- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
 WHERE discontinued <> 1 AND units_in_stock >= 20;
