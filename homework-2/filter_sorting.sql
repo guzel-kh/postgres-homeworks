7f38c13256bee38539eeb4a6352e175c90dfcb3a
guzel <guzel.kh23@gmail.ru>
2023-08-06 15:39:59 +0300

    fixed

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -6,8 +6,7 @@ WHERE ship_country IN ('France', 'Germany', 'Spain');
 
 -- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
 SELECT DISTINCT ship_city, ship_country
-FROM orders
-ORDER BY ship_country, ship_city;
+FROM orders;
 
 -- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
 SELECT AVG(shipped_date - order_date)
